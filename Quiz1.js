749d050e967aa670ecbbf7c12ab07858a49618b7
Malek Odibat <132284869+Malek1Alk7alil2@users.noreply.github.com>
2024-02-20 23:00:56 +0300

    Add files via upload

---
+++ Quiz1.js
@@ -0,0 +1,10 @@
+let num1 = 10
+let num2 = 20
+let num3 =5
+
+if(num3<num1){
+    if(num1<num2){
+       console.log("num2 is biggest " , num2)
+    }else 
+      console.log("num1 is biggest " , num3)
+}
